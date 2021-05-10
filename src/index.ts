ca5d104daac2eef9b07e974f02864eaf1422de39
jackgkk <gukalo2001@gmail.com>
2021-05-10 22:21:29 +0200

    final?

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import auth from './Routes/auth'
 import topItems from './Routes/topItems'
@@ -10,11 +10,14 @@ app.use(cors())
 app.use('/auth', auth)
 app.use('/topItems', topItems)
 app.use('/recommended', recommended)
+app.get('/', (req: Request, res: Response)=>{
+    res.send('Goou')
+})
 
 app.listen(port, ()=>{
     console.log("started on port "+port)
 })
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
+    app.use(express.static('../client/build'))
 }
