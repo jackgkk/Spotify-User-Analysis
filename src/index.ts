46a60346c199886e905e38aa49a81ce117244c24
jackgkk <gukalo2001@gmail.com>
2021-05-10 23:54:59 +0200

    final?

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import auth from './Routes/auth'
-import topItems from './Routes/topItems'
-import recommended from './Routes/getRecommended'
+import express, { Request, Response } from "express"
+import cors from "cors"
+import auth from "./Routes/auth"
+import topItems from "./Routes/topItems"
+import recommended from "./Routes/getRecommended"
 
 const port = process.env.PORT || 8080
+
 const app = express()
+
 app.use(cors())
-app.use('/auth', auth)
-app.use('/topItems', topItems)
-app.use('/recommended', recommended)
-app.get('/', (req: Request, res: Response)=>{
-    res.send('Goou')
+app.use("/auth", auth)
+app.use("/topItems", topItems)
+app.use("/recommended", recommended)
+app.get("/", (req: Request, res: Response) => {
+  res.send("Goou")
 })
 
-app.listen(port, ()=>{
-    console.log("started on port "+port)
+app.listen(port, () => {
+  console.log(`started on port ${port}`)
 })
 
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static('../client/build'))
+if (process.env.NODE_ENV === "production" || port) {
+  console.log('asignasi')
+  app.use(express.static("client/build"))
 }
