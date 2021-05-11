9d2462baf93d2c2f2dd51347af4e9a5efcb72614
jackgkk <gukalo2001@gmail.com>
2021-05-11 11:04:22 +0200

    final?

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,25 @@ import cors from "cors"
 import auth from "./Routes/auth"
 import topItems from "./Routes/topItems"
 import recommended from "./Routes/getRecommended"
+import path from "path"
 
 const port = process.env.PORT || 8080
-
 const app = express()
 
 app.use(cors())
 app.use("/auth", auth)
 app.use("/topItems", topItems)
 app.use("/recommended", recommended)
-app.get("/", (req: Request, res: Response) => {
-  res.send("Goou")
-})
+
 
 app.listen(port, () => {
   console.log(`started on port ${port}`)
 })
 
-if (process.env.NODE_ENV === "production" || port) {
-  console.log('asignasi')
-  app.use(express.static("client/build"))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")))
 }
+
+app.get("/", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../client/build", 'index.html'))
+})
