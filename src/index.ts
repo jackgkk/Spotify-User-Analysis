a5ce5473f01f338a718f22d24ab67e2e341f3fc1
jackgkk <gukalo2001@gmail.com>
2021-05-11 12:40:46 +0200

    final?

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")))
 }
 
-app.get("/", (req: Request, res: Response) => {
+app.get("*", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "../client/build", 'index.html'))
 })
